b5531ed53ca8ab3933e04853b73535a9b850dada
Noam Bigelman <noambigelman@gmail.com>
2021-08-24 14:45:37 +0300

    Implemented Create and New functionality

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ db.once("open", () => {
     console.log("Connected into Yelp Camp's database")
 })
 
+app.use(express.urlencoded({extended: true}))
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -26,6 +28,34 @@ app.get("/", (req, res) => {
     console.log("Another yelper joined the server")
     res.render("home.ejs")
 })
+
+// Todo: CRUD
+
+// get
+// Show all camps
+app.get("/camps", async (req, res) => {
+    const camps = await CampGroundModel.find({})
+    res.render('camps.ejs', {campgrounds: camps})
+})
+
+// Create & New
+// New
+// Create new camps page - get
+app.get("/camps/new", async (req, res) => {
+    res.render("editCamp")
+})
+// Create
+// Parse create new campground - post
+app.post("/camps", async (req,res) => {
+    console.log("Creating new camp:")
+    console.log(req.body)
+    const newCamp = new CampGroundModel({...req.body})
+    await newCamp.save()
+    res.redirect("/camps")
+})
+
+
+
 
 // Just for testing - this route will be removed
 app.get("/makecampground/:name", async (req, res) => {
