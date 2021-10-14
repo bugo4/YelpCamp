68b3b4967d2e7189b5ab197c01eacf84893b60cf
Noam Bigelman <bugo4office@gmail.com>
2021-10-14 16:18:10 +0300

    Implemented passport on the user schema

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose")
+const passportLocalMongoose = require("passport-local-mongoose")
 const Schema = mongoose.Schema;
 
 
 const UserSchema = new Schema({
-    username: {
+    email: {
         type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
+        required: true,
+        unique: true
     }
 })
+// Adds username and passport to User Schema
+UserSchema.plugin(passportLocalMongoose)
 
 UserSchema.statics.doesUserExist = async function(username) {
     return await this.find({ username })
