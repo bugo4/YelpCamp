d6008736545d5d3d9a16bdfd70637a6240227dd0
Noam Bigelman <bugo4office@gmail.com>
2021-10-21 17:57:05 +0300

    Implemented the review controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,29 +3,15 @@ const router = express.Router({mergeParams: true})
 const ReviewModel = require("../models/review")
 const CampGroundModel = require("../models/campground")
 
+const reviews = require("../controllers/reviews")
+
 const {isLoggedIn, isReviewAuthor} = require("../utils/middlewares") 
 
 
 
 /* Reviews CRUD */
 // Create
-router.post("/", isLoggedIn, async (req, res) => {
-    console.log("Here we go")
-    if (req.body.review == undefined) return;
-    console.log(req.params)
-    const ChosenReview = req.body.review
-    console.log(ChosenReview)
-    // if (ChosenReview.stars == undefined || ChosenReview.stars == undefined) return;
-    console.log("Trying to add a review")
-    console.log(ChosenReview)
-    const NewReview = new ReviewModel(ChosenReview);
-    NewReview.author = req.user._id
-    const campground = await CampGroundModel.findById(req.params.id);
-    campground.reviews.push(NewReview);
-    await NewReview.save();
-    await campground.save();
-    res.redirect(`/camps/${campground._id}`);
-})
+router.post("/", isLoggedIn, reviews.createReview)
 
 router.delete("/:reviewId", isLoggedIn, isReviewAuthor, async (req, res) => {
     const { id, reviewId } = req.params;
