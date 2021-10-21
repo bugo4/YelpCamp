4c955c07576052537020f2afd990542d703d44b0
Noam Bigelman <bugo4office@gmail.com>
2021-10-21 19:35:27 +0300

    Implemented a better look for routes

--- routes/users.js
+++ routes/users.js
@@ -10,12 +10,13 @@ const LocalStrategy = require("passport-local") // Save locally
 const {isLoggedIn} = require("../utils/middlewares") 
 
 // Authorization
-router.get("/login", users.renderUserLogin)
-router.post("/login", passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}) , users.loginUser)
+router.route("/login")
+    .get(users.renderUserLogin)
+    .post(passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}) , users.loginUser)
 
-router.get("/register", users.renderRegister)
-
-router.post("/register", users.registerUser)
+router.route("/register")
+    .get(users.renderRegister)
+    .post(users.registerUser)
 
 router.get("/logout", isLoggedIn, users.logoutUser)
 
